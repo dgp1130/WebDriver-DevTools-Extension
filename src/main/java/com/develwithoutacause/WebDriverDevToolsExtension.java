33071bae713597f360d185cb0b852e9b185bad09
Doug Parker <dgp1130@users.noreply.github.com>
2020-09-28 16:44:03 -0700

    Adds simple WebDriver execution.
    This starts WebDriver, loads `https://google.com/` and verifies that a `<body />` tag exists. This just shows that WebDriver is loaded successfully as a dependency.
    
    I opted to put the Linux Chromedriver binary directly in the project for simplicity.

--- src/main/java/com/develwithoutacause/WebDriverDevToolsExtension.java
+++ src/main/java/com/develwithoutacause/WebDriverDevToolsExtension.java
@@ -1,7 +1,19 @@
 package com.develwithoutacause;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class WebDriverDevToolsExtension {
   public static void main(final String[] args) {
-    System.out.println("Hello World!");
+    System.setProperty("webdriver.chrome.driver", "tools/chromedriver");
+    final var driver = new ChromeDriver(new ChromeOptions()
+        .setHeadless(true)
+    );
+    driver.get("https://www.google.com/");
+    final var element = driver.findElement(By.tagName("body"));
+    if (element == null) throw new Error("No <body /> tag!");
+    System.out.println(element);
+    driver.quit();
   }
 }
